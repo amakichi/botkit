4aeb71d13f612f911718cf09d38661f117f27b58
amakichi <km42.twfno@gmail.com>
2018-06-12 20:09:41 +0900

    add_test module

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -60,14 +60,6 @@ module.exports = function(controller) {
         bot.reply(message, 'test message');
     });
 
-    controller.hears(['^出勤', '^退勤'], 'direct_message', function(bot, message) {
-        if (message.match('出勤')) {
-            bot.reply(message, '出勤を記録しました。');
-        } else if (message.match('退勤')) {
-            bot.reply(message, '退勤を記録しました。');
-        }
-    });
-
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
