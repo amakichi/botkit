826c1d558617f609e1602b3394fbf5571f3e559c
amakichi <km42.twfno@gmail.com>
2018-06-12 20:12:43 +0900

    fix__

--- skills/kintai.js
+++ skills/kintai.js
@@ -1,11 +1,11 @@
 module.exports = function(controller) {
 
-  controller.hears(['^出勤', '^退勤'], 'direct_message', function(bot, message) {
-      if (message.match('出勤')) {
-          bot.reply(message, '出勤を記録しました。');
-      } else if (message.match('退勤')) {
-          bot.reply(message, '退勤を記録しました。');
-      }
+  controller.hears(['^出勤'], 'direct_message', function(bot, message) {
+    bot.reply(message, '出勤を記録しました。');
+  });
+
+  controller.hears(['^退勤'], 'direct_message', function(bot, message) {
+    bot.reply(message, '退勤を記録しました。');
   });
 
 };
